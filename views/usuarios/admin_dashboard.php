5555135b5df52bab84c8b93a1b21ee5bedb14481
Miguel Ángel <miguel.php@gmail.com>
2023-10-05 16:26:50 -0300

    Se corrigieron algunos problemas con el login
    ahora se puede hacer login y se puede visualizar datos del usuario registrado como su nombre y el nivel de usuario

--- views/usuarios/admin_dashboard.php
+++ views/usuarios/admin_dashboard.php
@@ -1,14 +1,46 @@
+<?php
+require_once '../../config/config.php';
+session_start(); // Iniciar la sesión
+
+// Función para obtener el nombre del usuario a partir del ID de usuario
+function obtenerNombreUsuario($conexion, $usuario_id) {
+    try {
+        $sql = "SELECT nombre FROM usuarios WHERE id = :usuario_id";
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindParam(':usuario_id', $usuario_id, PDO::PARAM_INT);
+        $stmt->execute();
+        $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        if ($resultado && isset($resultado['nombre'])) {
+            return $resultado['nombre'];
+        } else {
+            return "Usuario Desconocido";
+        }
+    } catch (PDOException $e) {
+        // Manejar errores de consulta
+        return "Error al obtener el nombre del usuario";
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel de Control del Administrador</title>
+    <title>Panel de Control del Empleado</title>
     <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
-    <h1>Panel de Control del Administrador</h1>
-    <p>Bienvenido, administrador. Aquí puedes gestionar usuarios y productos.</p>
-    <!-- Agrega aquí la funcionalidad y el contenido específico para el administrador -->
+<div class="navbar">
+    <span>Bienvenido, <?php echo obtenerNombreUsuario($conexion, $_SESSION['usuario_id']); ?></span>
+    <span>Nivel: <?php echo $_SESSION['nivel_permisos']; ?></span>
+    <a href="cerrar_sesion.php">Cerrar Sesión</a>
+</div>
+
+    <h1>Panel de Control del Empleado</h1>
+    <p>Aquí puedes buscar usuarios y gestionar productos.</p>
+    <!-- Agrega aquí la funcionalidad y el contenido específico para el empleado -->
 </body>
 </html>
--- views/usuarios/empleado_dashboard.php
+++ views/usuarios/empleado_dashboard.php
@@ -1,3 +1,27 @@
+<?php
+require_once '../../config/config.php';
+session_start(); // Iniciar la sesión
+// Función para obtener el nombre de usuario y nivel de permisos a partir del ID de usuario
+function obtenerNombreUsuario($conexion, $usuario_id) {
+    try {
+        $sql = "SELECT nombre FROM usuarios WHERE id = :usuario_id";
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindParam(':usuario_id', $usuario_id, PDO::PARAM_INT);
+        $stmt->execute();
+        $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($resultado && isset($resultado['nombre'])) {
+            return $resultado['nombre'];
+        } else {
+            return "Usuario Desconocido";
+        }
+    } catch (PDOException $e) {
+        // En caso de error de consulta, puedes manejarlo aquí
+        return "Error al obtener el nombre del usuario";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -7,8 +31,14 @@
     <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
+    <div class="navbar">
+        <span>Bienvenido, <?php echo obtenerNombreUsuario($conexion, $_SESSION['usuario_id']); ?></span>
+        <span>Nivel: <?php echo $_SESSION['nivel_permisos']; ?></span>
+        <a href="cerrar_sesion.php">Cerrar Sesión</a>
+    </div>
     <h1>Panel de Control del Empleado</h1>
-    <p>Bienvenido, empleado. Aquí puedes buscar usuarios y gestionar productos.</p>
+    <p>Aquí puedes buscar usuarios y gestionar productos.</p>
     <!-- Agrega aquí la funcionalidad y el contenido específico para el empleado -->
 </body>
 </html>
+
